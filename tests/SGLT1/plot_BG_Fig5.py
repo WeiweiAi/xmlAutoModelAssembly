5eb39c1a9c252cc8ee65b1d68fe63b324c4bbac7
Weiwei Ai <wai484@aucklanduni.ac.nz>
2024-04-30 09:08:36 +1200

    Plot fast binding in plot (b)

--- tests/SGLT1/plot_BG_Fig5.py
+++ tests/SGLT1/plot_BG_Fig5.py
@@ -14,8 +14,8 @@ filename_ss_fast= path_+'fig5_ss_fast.csv'
 
 
 subtitle_kwargs={}
-fig_cfg = {'num_rows': 1, 'num_cols': 1, 'width':6, 'height':4, 'fig_title': None, 'title_y': 0.98, 'fontsize': 10, 
-           'left': 0.145, 'bottom': 0.1, 'right': 0.9, 'top': 0.9, 'wspace': 0.2, 'hspace': 0.2}
+fig_cfg = {'num_rows': 1, 'num_cols': 2, 'width':8, 'height':4, 'fig_title': None, 'title_y': 0.98, 'fontsize': 10, 
+           'left': 0.1, 'bottom': 0.15, 'right': 0.95, 'top': 0.9, 'wspace': 0.28, 'hspace': 0.2}
 fig, axs= subplots_ajdust(fig_cfg, **subtitle_kwargs)
 
 plot_cfg = {}
@@ -28,12 +28,14 @@ line_cfg[2] = { 'xdata': (filename_BG,'V_E'), 'ydata':(filename_BG,'Ii'),
 line_cfg[3] = { 'xdata': (filename_ss,'V0_Vm'), 'ydata':(filename_ss,'Ii'),
                 'color': 'm', 'linestyle': '-.',  'legend_label': 'Steady state (37) from bond graph'}
 line_cfg[4] = { 'xdata': (filename_BG_fast,'V_E'), 'ydata':(filename_BG_fast,'Ii'),
-                'color': 'g', 'linestyle': '-.',  'legend_label': 'Bond graph-fast '}
+                'color': 'g', 'linestyle': '-.',  'legend_label': 'Bond graph-fast binding '}
 line_cfg[5] = { 'xdata': (filename_ss_fast,'V0_Vm'), 'ydata':(filename_ss_fast,'Ii'),
-                'color': 'b', 'linestyle': '-.',  'legend_label': 'Steady state (37) from bond graph-fast'}
+                'color': 'b', 'linestyle': '-.',  'legend_label': 'Steady-state Eqs 37 and 38' }
 
 
 plot_cfg[1] = {'ylabel': 'i (nA)', 'xlabel': 'V (mV)','show_grid': 'both', 'grid_axis': 'both', 
-                'line': [1,2,3,4,5], 'legend': [1,2,3,4,5]}
+                'line': [1,2], 'legend': [1,2]}
+plot_cfg[2] = {'ylabel': 'i (nA)', 'xlabel': 'V (mV)','show_grid': 'both', 'grid_axis': 'both', 
+                'line': [4,5], 'legend': [4,5]}
 
 plot_data(fig, axs, plot_cfg, line_cfg, save_fig)
